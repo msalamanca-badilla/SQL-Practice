d6678ee27207743d5b0f54d62eee8ab2749b31fe
Marianne <m.salamanca21@live.ca>
2022-01-24 14:35:19 -0500

    q4 complete

--- clues.sql
+++ clues.sql
@@ -31,8 +31,7 @@ SELECT * FROM countrylanguage WHERE language = 'Italian' AND percentage = 100;
 
 -- Write SQL query here
 
-
-
+SELECT * FROM city WHERE countrycode ='SMR';
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
